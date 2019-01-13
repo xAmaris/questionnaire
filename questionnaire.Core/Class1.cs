9891ea0cac480a66ed99025fa24c94fe2f9f2806
xAmaris <gabi97_97@o2.pl>
2019-01-13 16:43:35 +0100

    init

---
+++ questionnaire.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace questionnaire.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ questionnaire.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace questionnaire.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
